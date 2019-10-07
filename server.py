2857f0433a0f53c4c0a1b0751e9b4d3c6099c1c4
cokafor1 <cokafor1@slu.edu>
2019-10-07 17:10:55 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,44 +2,81 @@ import socket
 import ipaddress
 import sys
 import argparse
-import getopt
 import os
 import time
+import imghdr
 
-#Client address and port information-
-Address = '127.0.0.1'
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#Creating UDP socket-
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Internet is address family, UDP protocol
+clientSocket.setblocking(0)
 
-#def get file
-def getFile(file_name): #receive command, decode, check if file exists, open encode, send
-    print(file_name) #to remove
-    #serverSocket.sendto(("get %s" % file_name).encode(), anywhere)
+#defining functions to use in cases mapping. These functions do not do much, simply send string input file name to server.
+
+def getFile(file_name): #ask for file, if file doesn't exist, print message and always return to main()
+    print(locals())
+    try:
+        if os.path.exists(file_name) == True: # check if file in directory already
+            print("File already exists in directory. Maybe you should check before deciding to waste resources.")
+        else:
+            message = "get %s" % (file_name)  
+            clientSocket.sendto(message.encode(), server) #send get command to server
+            clientReceiveFile = open(file_name, 'wb')
+        
+            (incoming_file_size, rightthere) = clientSocket.recvfrom(2048) #why use new address?
+            file_size = int(incoming_file_size.decode()) #get incoming file size and serve as acknowledgment
+            print("Receiving file of size %s bytes." % file_size)
+
+            count = 0
+            totalpackets = (int(file_size) / 2048) #number of packets to sent = size of the file / buffer size (size of packets)
+            while count < totalpackets: #write to file
+                (serverReply, rightthere) = clientSocket.recvfrom(2048)
+                count += 1
+                clientReceiveFile.write(serverReply)
+            clientReceiveFile.close()
+
+        if os.path.getsize(clientReceiveFile.read()) != file_size: getFile(file_name)
+        else: main()
+    except socket.error:
+            print ("Took too long or something....")
+            main()
+    
+        
+
+def putFile(file_name): #send the file to server
     fileHandle = open(file_name, 'rb')
-
-    if os.path.exists(file_name) == True:  ##client needs to know about how much data to expect
-        file_size = str(os.path.getsize(file_name))
+    
+    if os.path.exists(file_name) == True:  
         try:
-            serverSocket.sendto(file_size.encode(), anywhere)
-        except OSError:
-                pass
+            message = "put %s" % (file_name)  
+            clientSocket.sendto(message.encode(), server) #send get command to server
+            time.sleep(1)
+        except socket.error:
+                print("I couldn't send the message.")
 
-        if os.path.getsize(file_name) < 2000:
+        file_size = str(os.path.getsize(file_name)) ##client needs to know about how much data to expect
+        try:
+            clientSocket.sendto(file_size.encode(), server)
+        except socket.error:
+                print("I couldn't send the file size.")
+
+        if os.path.getsize(file_name) < 2000: 
             new_fileHandle = fileHandle.read()
-            serverSocket.sendto(fileHandle, anywhere)
-            print(new_fileHandle)
+            clientSocket.sendto(new_fileHandle, server)
+            main()
         else:
             new_fileHandle = fileHandle.read(2048)
             count = 0
-            totalpackets = (file_size / 2048) #number of packets to sent = size of the file / buffer size (size of packets)
+            totalpackets = (int(file_size) / 2048) #number of packets to sent = size of the file / buffer size (size of packets)
             while count < totalpackets: 
-                serverSocket.sendto(new_fileHandle, anywhere)
-                count += 2048
-                print("%d packets sent (%d bytes)." % ((count/2048), (count/2048) * 2048))
+                clientSocket.sendto(new_fileHandle, server)
+                count += 1
+                print("%d packets sent (%d bytes)." % (count, (count * 2048)))
             new_fileHandle = fileHandle.read(2048)
             fileHandle.close()
+            main()
     else: 
         print('File does not exist in directory.')
-        serverSocket.sendto(("File is not found").encode(), anywhere) 
+        clientSocket.sendto(("File is not found").encode(), server) 
         try:
             newdir = print(input("You can specify the correct directory (Don't forget proper '\\' escape characters.)-"))
             os.chdir(newdir)
@@ -49,84 +86,97 @@ def getFile(file_name): #receive command, decode, check if file exists, open enc
             print("Sorry, there is an OSError and path couldn't be changed.")
             main()
 
+def renameFile(old_file_name, new_file_name): #rename the files using two arguments
+    message = "rename %s %s" % (old_file_name, new_file_name)  
+    clientSocket.sendto(message.encode(), server)
+    time.sleep(1)
+    try:
+        (serverReply, server) = clientSocket.recvfrom(2408)
+        print(serverReply, '\n', sep = '')
+    except socket.error:
+        print ("Took too long....")
+        main()
+    main()
 
-#def put file: 
-def putFile(file_name): #receive command, decode, check if file exists, open encode, send
-    (file_name, (Address,Trop)) = serverSocket.recvfrom(6000)
-    sFile = file_name.decode()
-    if os.path.exits(sFile) == False: 
-        fileHandle = open(sFile, 'rb')
-        fileHandle.write(sFile, 'wb')
-        fileHandle.close()
-        serverSocket.sendto(fileHandle.encode(), Address)
-    else: 
-        reply = "File already exists in directory."
-        serverSocket.sendto(reply.encode(), Address)
+def listFiles(): 
+    message = "list"
+    clientSocket.sendto(message.encode(), server)
+    time.sleep(1)
+    (serverReply, somewhere) = clientSocket.recvfrom(2048)
+    print(serverReply.decode(), '\n', sep = '')
+    main()
+    
 
-#def rename file:
-def renameFile(old_file_name, new_file_name): #receive, decode, and rename on server.
-    (old_file_name, (Address,Trop)) = serverSocket.recvfrom(6000)
-    (new_file_name, (Address,Trop)) = serverSocket.recvfrom(6000)
-    oldFile = old_file_name.decode()
-    newFile = new_file_name.decode()
-    os.rename(oldFile, newFile)
-    serverSocket.sendto(newFile.encode(), Address)
-
-#def list:
-def listFiles():
-    listing = os.listdir()
-    for x in listing:
-        serverSocket.sendto(x.encode(), anywhere)
-        time.sleep(1)
-
-#def exit:
-def exit():
-    print('POOP')
+def exit(): #simply send 'exit' string to server and allow it to handle
+    message = "exit"
+    clientSocket.sendto(message.encode(), server)
+    clientSocket.close()
+    print('Gracefully exiting.....')
+    time.sleep(.5)
     sys.exit()
 
-def main():
 
-    parse = argparse.ArgumentParser(prog = 'server')
-    parse.add_argument('Port', nargs = 1, type = int, help = "Enter port number above 1024.")
+def action(): #how to retrieve arguments......there should be a better way.
     
+    print("Your options are the following-")
+    print("get [file_name]")
+    print("put [file_name]")
+    print("rename [old_file_name] [new_file_name]")
+    print("list")
+    print("exit")
+
+    action = input('What would you like to do?').split()
+
+    if len(action) >= 4:#use loop to append command line user inputs to action list. Make list no more than four values
+       print('This program only supports one command and up to two arguments, but you can try again.')
+       action()
+    return action
+
+def main():
+    print()
+    print()
+
+    #Server address and port information-
+    parse = argparse.ArgumentParser(prog = 'client')
+    parse.add_argument('Address', nargs = 1, type = ipaddress.ip_address, help = "Enter IP Address.")
+    parse.add_argument('Port', nargs = 1, type = int, help = "Enter port number above 1024.")
+        
     args = parse.parse_args()
 
-    Port = args.Port[0]
+ 
 
-    serverSocket.bind(("", Port))
+    global server
+    server = (args.Address[0]._explode_shorthand_ip_string(), args.Port[0])
 
-    #a dictionary, using keys to execute desired function specified in action variable #if message received is x, jump to function
+    choice = action()
+    command = str(choice[0])
+
+    #a dictionary, using keys to execute desired function specified in action variable
     options = {
     'get': getFile,
     'put': putFile,
     'rename': renameFile,
     'list': listFiles,
     'exit': exit } #needs to have user inputs from the command line
-    
-    
-    while True:
-        print("Ready!")
 
-        global anywhere
-        (message, anywhere) = serverSocket.recvfrom(2048)
+    if command not in options: 
+        print("Not in options but I will give to server to handle. Trust me......")
+        #clientSocket.sendto(command.encode(), server)
+        #time.sleep(2)
+        #(error, server) = clientSocket.recvfrom(2048)
+        #print(error.decode())
+        #print(1)
+        main()
+    elif len(choice) == 2:
+        file_name = choice[1]
+        options.get(command)(file_name)
+    elif len(choice) == 3:
+        old_file_name = choice[1]
+        new_file_name = choice[2]
+        options.get(command)(old_file_name, new_file_name )
+    else: options.get(command)()
 
-        parse_msg = message.decode().split()
-        command = parse_msg[0]
+    #clientSocket.close()
 
-        if command not in options: 
-            serverSocket.sendto(("Client is right, I haven't been told how to handle this command. Sorry." % command).encode(), anywhere) #how do I know this for client address?
-            break
-        elif len(parse_msg) == 2:
-            file_name = parse_msg[1]
-            options.get(command)(file_name)
-        elif len(parse_msg) == 3:
-            old_file_name = parse_msg[1].encode()
-            new_file_name = parse_msg[2].encode()
-            options.get(command)(old_file_name, new_file_name)
-        else: options.get(command)()
-
-        
-    
 if __name__ == "__main__": 
     main()
-    
